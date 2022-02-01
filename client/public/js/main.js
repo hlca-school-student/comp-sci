c8a44af27e6185384871c11b99486c37171a6b09
adamglang <adam@getlabs.com>
2022-02-01 10:06:09 -0800

    we have data now

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -2,9 +2,28 @@ async function get() {
     try {
         const listedHuntsStream = await fetch('http://localhost:5050/list');
         const listedHunts = await listedHuntsStream.json();
-        console.log(listedHunts);
+        writeHunts(listedHunts);
     } catch(e) {
         console.error(e);
     }
 }
 
+function writeHunts(listedHunts) {
+    const tableRoot = document.getElementById('tableRoot');
+
+    if (listedHunts.rows.length) {
+        listedHunts.rows.forEach((hunt) => {
+            tableRoot.innerHTML += `
+                <tr>
+                    <td>${hunt.tag}</td>
+                    <td>${hunt.open}</td>
+                    <td>${hunt.close}</td>
+                    <td>${hunt.ornament}</td>
+                    <td>${hunt.game}</td>
+                    <td>${hunt.method}</td>
+                    <td>${hunt.area}</td>
+                </tr>
+            `;
+        })
+    }
+}
